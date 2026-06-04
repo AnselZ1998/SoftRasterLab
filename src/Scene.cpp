06f6b6e7fb2c5d0d57357dd82e97645fed5397af
zhaijiafeng <zhaijiafeng@phenom-films.com>
2026-06-04 19:09:34 +0800

    add obj_loader & and mouse_control

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,19 +1,168 @@
 #include "Scene.h"
 #include <iostream>
+#include "OBJ_Loader.h"
 
 static int gWidth = 0;
 static int gHeight = 0;
+static objl::Loader gObjLoader;
+static bool gModelLoaded = false;
+static float gModelScale = 0.01f;
+static float gModelOffsetX = 0.0f;
+static float gModelOffsetY = 0.0f;
 
 void InitScene(int inViewportWidth, int inViewportHeight) {
 	gWidth = inViewportWidth;
 	gHeight = inViewportHeight;
-    #ifdef _DEBUG
-        TestVector4();
-    #endif
+
+    //Load model
+    LoadModel();
 }
 
-static void TestVector4() {
-   
+Vector3 ToVector3(const objl::Vector3& p)
+{
+    return Vector3(p.X, p.Y, p.Z);
+}
+
+void LoadModel() {
+    // Load model
+    gModelLoaded = gObjLoader.LoadFile("model/human_t.obj");
+
+    if (!gModelLoaded)
+    {
+        std::cout << "Failed to load obj model." << std::endl;
+    }
+    else
+    {
+        std::cout << "OBJ loaded. Mesh count: "
+            << gObjLoader.LoadedMeshes.size()
+            << std::endl;
+    }
+}
+
+void RenderOneFrame(float inFrameTime) {
+    // TODO: ClearScreen();
+
+    // 1. Model Matrix
+    Matrix4 scaleMatrix;
+    scaleMatrix.SetScale(gModelScale, gModelScale, gModelScale);
+
+    Matrix4 translateMatrix;
+    translateMatrix.SetTranslation(gModelOffsetX, gModelOffsetY, 4.0f);
+
+    Matrix4 modelMatrix = translateMatrix * scaleMatrix;
+
+    // 2. View Matrix
+    Matrix4 viewMatrix;
+    viewMatrix.SetTranslation(0.0f, 0.0f, 10.0f);
+
+    // 3. Projection Matrix
+    Matrix4 projectionMatrix;
+
+    float fov = 45.0f * 3.1415926f / 180.0f;
+    float aspect = static_cast<float>(gWidth) / static_cast<float>(gHeight);
+
+    projectionMatrix.SetPerspective(
+        fov,
+        aspect,
+        0.1f,
+        100.0f
+    );
+
+    // 4. MVP
+    Matrix4 mvp = projectionMatrix * viewMatrix * modelMatrix;
+
+    // 5. Render
+    if (!gModelLoaded)
+    {
+        return;
+    }
+
+    int triangleCount = 0;
+    const int maxTriangleCount = 50000;
+
+
+    for (const objl::Mesh& mesh : gObjLoader.LoadedMeshes)
+    {
+        const std::vector<objl::Vertex>& vertices = mesh.Vertices;
+        const std::vector<unsigned int>& indices = mesh.Indices;
+
+        for (size_t i = 0; i + 2 < indices.size(); i += 3)
+        {
+            // debug max
+            if (triangleCount >= maxTriangleCount)
+            {
+                return;
+            }
+            const objl::Vertex& v0 = vertices[indices[i + 0]];
+            const objl::Vertex& v1 = vertices[indices[i + 1]];
+            const objl::Vertex& v2 = vertices[indices[i + 2]];
+
+            Vector3 A = ToVector3(v0.Position);
+            Vector3 B = ToVector3(v1.Position);
+            Vector3 C = ToVector3(v2.Position);
+
+            DrawTriangle(A, B, C, mvp);
+
+            triangleCount++;
+        }
+    }
+}
+
+void DrawTriangle(
+    const Vector3& SA,
+    const Vector3& SB,
+    const Vector3& SC,
+    const Matrix4& mvp
+)
+{
+    Vector4 SA4 = Vector4::FromPoint(SA);
+    Vector4 SB4 = Vector4::FromPoint(SB);
+    Vector4 SC4 = Vector4::FromPoint(SC);
+
+    Vector4 AClip = mvp * SA4;
+    Vector4 BClip = mvp * SB4;
+    Vector4 CClip = mvp * SC4;
+
+    Vector4 ANDC = PerspectiveDivide(AClip);
+    Vector4 BNDC = PerspectiveDivide(BClip);
+    Vector4 CNDC = PerspectiveDivide(CClip);
+
+    RenderTriangle(ANDC, BNDC, CNDC);
+}
+
+void RenderTriangle(const Vector4& A, const Vector4& B, const Vector4& C)
+{
+    Vector4 ASS(A), BSS(B), CSS(C);
+
+    // NDC -> Screen Space
+    // NDC *= 0.5f -> += 0.5f -> *gCanvas -1
+
+    ASS.x = (ASS.x + 1.0f) * 0.5f * gWidth - 1;
+    ASS.y = (ASS.y + 1.0f) * 0.5f * gHeight - 1;
+
+    BSS.x = (BSS.x + 1.0f) * 0.5f * gWidth - 1;
+    BSS.y = (BSS.y + 1.0f) * 0.5f * gHeight - 1;
+
+    CSS.x = (CSS.x + 1.0f) * 0.5f * gWidth - 1;
+    CSS.y = (CSS.y + 1.0f) * 0.5f * gHeight - 1;
+
+    BoundingVolume BV = BoundingVolume();
+    BV.GetBoundingVolume(ASS, BSS, CSS);
+
+    int minX_i = floor(BV.minX);
+    int minY_i = floor(BV.minY);
+    int maxX_i = ceil(BV.maxX);
+    int maxY_i = ceil(BV.maxY);
+
+    for (int x = minX_i; x < maxX_i; x++)
+    {
+        for (int y = minY_i; y < maxY_i; y++)
+        {
+            if (IsInTriangle(x, y, ASS, BSS, CSS)) {
+                SetPixelColor(x, y, 255, 200, 80, 255);
+            }
+        }
+    }
 }
 
 bool IsInTriangle(int inX, int inY, const Vector4& inA, const Vector4& inB, const Vector4& inC) {
@@ -41,97 +190,6 @@ bool IsInTriangle(int inX, int inY, const Vector4& inA, const Vector4& inB, cons
     return sameSide;
 }
 
-void RenderTriangle(const Vector4& A, const Vector4& B, const Vector4& C)
-{
-    Vector4 ASS(A), BSS(B), CSS(C);
-    
-    // NDC -> Screen Space
-    // NDC *= 0.5f -> += 0.5f -> *gCanvas -1
-
-    ASS.x = (ASS.x + 1.0f) * 0.5f * gWidth - 1;
-    ASS.y = (ASS.y + 1.0f) * 0.5f * gHeight - 1;
-
-    BSS.x = (BSS.x + 1.0f) * 0.5f * gWidth - 1;
-    BSS.y = (BSS.y + 1.0f) * 0.5f * gHeight - 1;
-
-    CSS.x = (CSS.x + 1.0f) * 0.5f * gWidth - 1;
-    CSS.y = (CSS.y + 1.0f) * 0.5f * gHeight - 1;
-
-    BoundingVolume BV = BoundingVolume();
-    BV.GetBoundingVolume(ASS, BSS, CSS);
-    
-    int minX_i = floor(BV.minX);
-    int minY_i = floor(BV.minY);
-    int maxX_i = ceil(BV.maxX);
-    int maxY_i = ceil(BV.maxY);
-
-    for (int x = minX_i; x < maxX_i; x++)
-    {
-        for (int y = minY_i; y < maxY_i; y++)
-        {
-            if (IsInTriangle(x, y, ASS, BSS, CSS)) {
-                SetPixelColor(x, y, 255, 200, 80, 255);
-            }
-
-        }
-    }
-}
-
-void DrawTriangle(const Vector3& SA, const Vector3& SB, const Vector3& SC) {
-    // vec3 -> vec4
-    Vector4 SA4 = Vector4::FromPoint(SA);
-    Vector4 SB4 = Vector4::FromPoint(SB);
-    Vector4 SC4 = Vector4::FromPoint(SC);
-
-    // camera
-    Vector4 cameraPos = { 0.0f, 0.0f, -10.0f, 1.0f };
-    float fov = 45.0f * 3.1415926f / 180.0f;
-
-    // View Matrix
-    Matrix4 m4View;
-    m4View.SetTranslation(
-        -cameraPos.x,
-        -cameraPos.y,
-        -cameraPos.z
-    );
-
-    // 1. World Space -> View Space
-    Vector4 AView = m4View * SA4;
-    Vector4 BView = m4View * SB4;
-    Vector4 CView = m4View * SC4;
-
-    // Projection Matrix
-    Matrix4 m4Pro;
-    m4Pro.SetPerspective(
-        fov,
-        1.0f,
-        0.1f,
-        100.0f
-    );
-
-    // 2. View Space -> Clip Space
-    Vector4 AClip = m4Pro * AView;
-    Vector4 BClip = m4Pro * BView;
-    Vector4 CClip = m4Pro * CView;
-
-    // 3. Perspective Divide: Clip -> NDC
-    Vector4 ANDC = PerspectiveDivide(AClip);
-    Vector4 BNDC = PerspectiveDivide(BClip);
-    Vector4 CNDC = PerspectiveDivide(CClip);
-
-    // 4. NDC -> Screen + Rasterization
-    RenderTriangle(ANDC, BNDC, CNDC);
-}
-
-void RenderOneFrame(float inFrameTime) {
-    // Origin Triangle Point
-    Vector3 SA = { -1.0f, -0.8f, 4.0f};
-    Vector3 SB = { 1.2f, -0.6f, 6.0f};
-    Vector3 SC = { 0.0f,  1.0f, 13.0f};
-
-    DrawTriangle(SA, SB, SC);
-}
-
 Vector4 PerspectiveDivide(const Vector4& v)
 {
     return Vector4(
@@ -140,4 +198,34 @@ Vector4 PerspectiveDivide(const Vector4& v)
         v.z / v.w,
         1.0f
     );
+}
+
+void OnMouseWheel(int wheelDelta)
+{
+    if (wheelDelta > 0)
+    {
+        gModelScale *= 1.1f;
+    }
+    else
+    {
+        gModelScale *= 0.9f;
+    }
+
+    if (gModelScale < 0.0001f)
+    {
+        gModelScale = 0.0001f;
+    }
+
+    if (gModelScale > 10.0f)
+    {
+        gModelScale = 10.0f;
+    }
+}
+
+void OnMouseDrag(int dx, int dy)
+{
+    float moveSpeed = 0.01f;
+
+    gModelOffsetX += dx * moveSpeed;
+    gModelOffsetY -= dy * moveSpeed;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,101 +1,243 @@
-#include<windows.h>
+#include <windows.h>
+#include <windowsx.h>
+
 #include "Scene.h"
 
-static int gCanvasWidth = 0, gCanvasHeight = 0;
+static int gCanvasWidth = 0;
+static int gCanvasHeight = 0;
+
 static HDC gWindowCompatibleDC = nullptr;
 static HBITMAP gColorBitmap = nullptr;
 static unsigned int* gBackgroundColorBuffer = nullptr;
 static unsigned short* gBackgroundDepthBuffer = nullptr;
-LARGE_INTEGER frequency;
-LARGE_INTEGER startTime;
-LARGE_INTEGER endTime;
 
-LRESULT CALLBACK RTWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    switch (msg) {
+static bool gIsDragging = false;
+static int gLastMouseX = 0;
+static int gLastMouseY = 0;
+
+static LARGE_INTEGER gFrequency;
+static LARGE_INTEGER gPreviousTime;
+
+LRESULT CALLBACK RTWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
     case WM_CLOSE:
+    {
         PostQuitMessage(0);
-        break;
+        return 0;
     }
-    return DefWindowProc(hwnd, msg, wParam, lParam);
+
+    case WM_MOUSEWHEEL:
+    {
+        int delta = GET_WHEEL_DELTA_WPARAM(wParam);
+        OnMouseWheel(delta);
+        return 0;
+    }
+
+    case WM_LBUTTONDOWN:
+    {
+        gIsDragging = true;
+
+        gLastMouseX = GET_X_LPARAM(lParam);
+        gLastMouseY = GET_Y_LPARAM(lParam);
+
+        SetCapture(hwnd);
+        return 0;
+    }
+
+    case WM_MOUSEMOVE:
+    {
+        if (gIsDragging)
+        {
+            int x = GET_X_LPARAM(lParam);
+            int y = GET_Y_LPARAM(lParam);
+
+            int dx = x - gLastMouseX;
+            int dy = y - gLastMouseY;
+
+            OnMouseDrag(dx, dy);
+
+            gLastMouseX = x;
+            gLastMouseY = y;
+        }
+
+        return 0;
+    }
+
+    case WM_LBUTTONUP:
+    {
+        gIsDragging = false;
+        ReleaseCapture();
+        return 0;
+    }
+
+    default:
+    {
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    }
 }
-INT WINAPI WinMain(HINSTANCE inHInstance, HINSTANCE, LPSTR inCommandLine, int) {
+
+INT WINAPI WinMain(HINSTANCE inHInstance, HINSTANCE, LPSTR inCommandLine, int)
+{
     gCanvasWidth = 640;
     gCanvasHeight = 360;
 
-    QueryPerformanceFrequency(&frequency);
+    QueryPerformanceFrequency(&gFrequency);
+    QueryPerformanceCounter(&gPreviousTime);
 
-    //register window
-    WNDCLASSEX wndclass;
-    wndclass.cbClsExtra = 0;
-    wndclass.cbSize = sizeof(WNDCLASSEX);
-    wndclass.cbWndExtra = 0;
-    wndclass.hbrBackground = NULL;
-    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wndclass.hIcon = NULL;
-    wndclass.hIconSm = NULL;
-    wndclass.hInstance = inHInstance;
-    wndclass.lpfnWndProc = RTWindowProc;
-    wndclass.lpszClassName = L"RTWindow";
-    wndclass.lpszMenuName = NULL;
+    // Register window class
+    WNDCLASSEXW wndclass{};
+    wndclass.cbSize = sizeof(WNDCLASSEXW);
     wndclass.style = CS_VREDRAW | CS_HREDRAW;
-    ATOM atom = RegisterClassEx(&wndclass);
-    if (!atom) {
+    wndclass.lpfnWndProc = RTWindowProc;
+    wndclass.hInstance = inHInstance;
+    wndclass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wndclass.lpszClassName = L"RTWindow";
+
+    ATOM atom = RegisterClassExW(&wndclass);
+    if (!atom)
+    {
         return 0;
     }
-    RECT rect = { 0,0,gCanvasWidth,gCanvasHeight };
+
+    RECT rect = { 0, 0, gCanvasWidth, gCanvasHeight };
     AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+
     int viewportWidth = rect.right - rect.left;
     int viewportHeight = rect.bottom - rect.top;
-    //create window
-    HWND hwnd = CreateWindowEx(NULL, L"RTWindow", L"Alice Rendering Window", WS_OVERLAPPEDWINDOW,
-        100, 100, viewportWidth, viewportHeight, NULL, NULL, inHInstance, NULL);
-    //show window
+
+    HWND hwnd = CreateWindowExW(
+        0,
+        L"RTWindow",
+        L"Alice Rendering Window",
+        WS_OVERLAPPEDWINDOW,
+        100,
+        100,
+        viewportWidth,
+        viewportHeight,
+        nullptr,
+        nullptr,
+        inHInstance,
+        nullptr
+    );
+
+    if (!hwnd)
+    {
+        return 0;
+    }
+
     ShowWindow(hwnd, SW_SHOW);
     UpdateWindow(hwnd);
+
     HDC windowCanvasDC = GetDC(hwnd);
     gWindowCompatibleDC = CreateCompatibleDC(windowCanvasDC);
 
-    BITMAPINFO	bmpInfor;
-    bmpInfor.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-    bmpInfor.bmiHeader.biWidth = gCanvasWidth;
-    bmpInfor.bmiHeader.biHeight = gCanvasHeight;
-    bmpInfor.bmiHeader.biPlanes = 1;
-    bmpInfor.bmiHeader.biBitCount = 32;
-    bmpInfor.bmiHeader.biCompression = BI_RGB;
-    bmpInfor.bmiHeader.biSizeImage = 0;
-    bmpInfor.bmiHeader.biXPelsPerMeter = 0;
-    bmpInfor.bmiHeader.biYPelsPerMeter = 0;
-    bmpInfor.bmiHeader.biClrUsed = 0;
-    bmpInfor.bmiHeader.biClrImportant = 0;
+    BITMAPINFO bmpInfo{};
+    bmpInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmpInfo.bmiHeader.biWidth = gCanvasWidth;
+    bmpInfo.bmiHeader.biHeight = gCanvasHeight;
+    bmpInfo.bmiHeader.biPlanes = 1;
+    bmpInfo.bmiHeader.biBitCount = 32;
+    bmpInfo.bmiHeader.biCompression = BI_RGB;
 
-    gColorBitmap = CreateDIBSection(windowCanvasDC, &bmpInfor, DIB_RGB_COLORS, (void**)&gBackgroundColorBuffer, 0, 0);
+    gColorBitmap = CreateDIBSection(
+        windowCanvasDC,
+        &bmpInfo,
+        DIB_RGB_COLORS,
+        reinterpret_cast<void**>(&gBackgroundColorBuffer),
+        nullptr,
+        0
+    );
+
     SelectObject(gWindowCompatibleDC, gColorBitmap);
+
     gBackgroundDepthBuffer = new unsigned short[gCanvasWidth * gCanvasHeight];
+
     InitScene(gCanvasWidth, gCanvasHeight);
-    MSG msg;
-    while (true) {
-        if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE)) {
-            if (msg.message == WM_QUIT) {
+
+    MSG msg{};
+
+    while (true)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+            {
                 break;
             }
+
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
-        QueryPerformanceCounter(&startTime);
-        //debug mode
-        RenderOneFrame(0.0f);
-        QueryPerformanceCounter(&endTime);
-        float deltaTime = (float)(endTime.QuadPart - startTime.QuadPart) / (float)frequency.QuadPart;
-        //std::cout << "deltaTime: " << deltaTime  << "s" << std::endl;
-        BitBlt(windowCanvasDC, 0, 0, gCanvasWidth, gCanvasHeight, gWindowCompatibleDC, 0, 0, SRCCOPY);
+
+        LARGE_INTEGER currentTime;
+        QueryPerformanceCounter(&currentTime);
+
+        float deltaTime =
+            static_cast<float>(currentTime.QuadPart - gPreviousTime.QuadPart) /
+            static_cast<float>(gFrequency.QuadPart);
+
+        gPreviousTime = currentTime;
+
+        RenderOneFrame(deltaTime);
+
+        BitBlt(
+            windowCanvasDC,
+            0,
+            0,
+            gCanvasWidth,
+            gCanvasHeight,
+            gWindowCompatibleDC,
+            0,
+            0,
+            SRCCOPY
+        );
+
         Sleep(1);
     }
-}
-void SetPixelColor(int inX, int inY, unsigned char inR, unsigned char inG, unsigned char inB, unsigned char inA) {
-    unsigned int color = (inA << 24) + (inR << 16) + (inG << 8) + inB;
-    int nPixelCount = gCanvasWidth * gCanvasHeight;
-    int nPixelIndex = inX + inY * gCanvasWidth;
-    if (nPixelIndex < nPixelCount) {
-        gBackgroundColorBuffer[nPixelIndex] = color;
+
+    delete[] gBackgroundDepthBuffer;
+    gBackgroundDepthBuffer = nullptr;
+
+    if (gColorBitmap)
+    {
+        DeleteObject(gColorBitmap);
+        gColorBitmap = nullptr;
     }
+
+    if (gWindowCompatibleDC)
+    {
+        DeleteDC(gWindowCompatibleDC);
+        gWindowCompatibleDC = nullptr;
+    }
+
+    ReleaseDC(hwnd, windowCanvasDC);
+
+    return 0;
+}
+
+void SetPixelColor(
+    int inX,
+    int inY,
+    unsigned char inR,
+    unsigned char inG,
+    unsigned char inB,
+    unsigned char inA
+)
+{
+    if (inX < 0 || inX >= gCanvasWidth || inY < 0 || inY >= gCanvasHeight)
+    {
+        return;
+    }
+
+    unsigned int color =
+        (static_cast<unsigned int>(inA) << 24) |
+        (static_cast<unsigned int>(inR) << 16) |
+        (static_cast<unsigned int>(inG) << 8) |
+        static_cast<unsigned int>(inB);
+
+    int pixelIndex = inX + inY * gCanvasWidth;
+    gBackgroundColorBuffer[pixelIndex] = color;
 }
